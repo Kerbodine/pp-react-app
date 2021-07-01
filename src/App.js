18558d5e21bdfe501bdb9bb028e1185300d1cdd4
Michael Tong <michaelyuyangtong@gmail.com>
2021-07-01 12:49:03 +0800

    Added navbar css file in project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 
-import Navbar from "./components/Navbar";
+import Navbar from "./components/nav-bar/Navbar";
 
 function App() {
 
--- src/components/nav-bar/Navbar.js
+++ src/components/nav-bar/Navbar.js
@@ -1,18 +1,18 @@
 import React from "react";
 // import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
 import SideNav, { NavItem, NavIcon, NavText } from "@trendmicro/react-sidenav";
-import "@trendmicro/react-sidenav/dist/react-sidenav.css";
+import "./Navbar.css"
 import { MdDashboard, MdAccountBox } from "react-icons/md";
 import { BiCalendar, BiCheckSquare, BiNote, BiWrench } from "react-icons/bi";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Dashboard from "./Dashboard";
-import Profile from "./Profile";
-import Calendar from "./Calendar";
-import Reminders from "./Reminders";
-import Notes from "./Notes";
-import Tools from "./Tools";
-import Settings from "./Settings";
+import Dashboard from "../Dashboard";
+import Profile from "../Profile";
+import Calendar from "../Calendar";
+import Reminders from "../Reminders";
+import Notes from "../Notes";
+import Tools from "../Tools";
+import Settings from "../Settings";
 
 export default function Navbar() {
   const iconStyles = {
@@ -39,49 +39,37 @@ export default function Navbar() {
                 <SideNav.Nav defaultSelected="/">
                   <NavItem eventKey="/">
                     <NavIcon>
-                      <i>
-                        <MdDashboard style={iconStyles} />
-                      </i>
+                      <i><MdDashboard style={iconStyles} title="Dashboard"/></i>
                     </NavIcon>
-                    <NavText>Home</NavText>
+                    <NavText>Dashboard</NavText>
                   </NavItem>
                   <NavItem eventKey="/profile">
                     <NavIcon>
-                      <i>
-                        <MdAccountBox style={iconStyles} />
-                      </i>
+                      <i><MdAccountBox style={iconStyles} /></i>
                     </NavIcon>
                     <NavText>Profile</NavText>
                   </NavItem>
                   <NavItem eventKey="/calendar">
                     <NavIcon>
-                      <i>
-                        <BiCalendar style={iconStyles} />
-                      </i>
+                      <i><BiCalendar style={iconStyles} /></i>
                     </NavIcon>
                     <NavText>Calendar</NavText>
                   </NavItem>
                   <NavItem eventKey="/reminders">
                     <NavIcon>
-                      <i>
-                        <BiCheckSquare style={iconStyles} />
-                      </i>
+                      <i><BiCheckSquare style={iconStyles} /></i>
                     </NavIcon>
                     <NavText>Reminders</NavText>
                   </NavItem>
                   <NavItem eventKey="/notes">
                     <NavIcon>
-                      <i>
-                        <BiNote style={iconStyles} />
-                      </i>
+                      <i><BiNote style={iconStyles} /></i>
                     </NavIcon>
                     <NavText>Notes</NavText>
                   </NavItem>
                   <NavItem eventKey="/tools">
                     <NavIcon>
-                      <i>
-                        <BiWrench style={iconStyles} />
-                      </i>
+                      <i><BiWrench style={iconStyles} /></i>
                     </NavIcon>
                     <NavText>Tools</NavText>
                   </NavItem>
