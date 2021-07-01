915ac5087319c876dc91aa425d03984719cc5b83
Michael Tong <michaelyuyangtong@gmail.com>
2021-07-01 13:09:19 +0800

    Added editor js text editor on notes page

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import TextEditor from './TextEditor'
 
 export default function Notes() {
   return (
     <div>
       <h1>Notes Page</h1>
+      <div className="editor" style={{ marginLeft: "100px", marginRight: "100px"}}>
+        <TextEditor />
+      </div>
     </div>
   )
 }
