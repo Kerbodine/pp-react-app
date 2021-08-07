4154473dfb4e684f6f8d0c8c01336a8f84096a76
Michael Tong <michaelyuyangtong@gmail.com>
2021-08-07 08:54:30 +0800

    Added correct color values for notes page.

--- src/components/notes/Notes.js
+++ src/components/notes/Notes.js
@@ -4,9 +4,8 @@ import TagList from "./TagList";
 import PageSidebar from "./PageSidebar";
 
 export default function Notes() {
-
   return (
-    <div className="h-screen w-full bg-primary-800 flex">
+    <div className="h-screen w-full bg-primary dark:bg-primary-800 flex">
       <div className="w-full h-auto mr-4 mt-20 mb-4 lg:mr-0">
         <div className="h-full rounded-2xl overflow-hidden shadow-md transition-width flex flex-row ">
           <div className="h-full">
@@ -17,7 +16,14 @@ export default function Notes() {
           <div className="w-full flex flex-col">
             <div className="">
               <div className="px-8 pt-8 bg-primary-100 flex flex-col">
-                <input id="document-title" autoComplete="off" className="h-12 bg-transparent text-primary-800 font-bold outline-none text-4xl mb-4" placeholder="Untitled" type="text" aria-label="document title"></input>
+                <input
+                  id="document-title"
+                  autoComplete="off"
+                  className="h-12 bg-transparent text-primary-800 font-bold outline-none text-4xl mb-4"
+                  placeholder="Untitled"
+                  type="text"
+                  aria-label="document title"
+                ></input>
                 <TagList />
                 <div className="w-full h-4 bg-primary-100"></div>
               </div>
