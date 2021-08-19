dcfd6240e363f07f7cfba9806229fa54e4f7fb1f
Michael Tong <michaelyuyangtong@gmail.com>
2021-08-19 20:39:44 +0800

    Added tfocaltimer beta build version

--- src/components/Dashboard.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Dashboard() {
-  return (
-    <div>
-      <h1>Dashboard Page</h1>
-    </div>
-  );
-}
--- src/components/Extras.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Extras() {
-  return (
-    <div>
-      <h1>Extras Page</h1>
-    </div>
-  );
-}
